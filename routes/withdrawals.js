4d4d54eef7edfd4394c8d490a5af23d1530a91f7
racie7 <graciamuts@gmail.com>
2025-04-13 16:28:00 +0300

    test

--- routes/withdrawals.js
+++ routes/withdrawals.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const WithdrawalsController = require('../controllers/WithdrawalsController');
 
 router.post('/nd', WithdrawalsController.store);
-router.get('/nd:phoneNumber', WithdrawalsController.getWithdrawals);
+router.get('/nd/:phoneNumber', WithdrawalsController.getWithdrawals);
 router.get('/nd-total/:phoneNumber', WithdrawalsController.getTotalWithdrawalsByUser);
 
 module.exports = router;
